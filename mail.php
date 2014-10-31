c384e686a8ae8c78dfbc27aec4df0ad5399e6737
Ivan Demenkov <ivan@demenkov.com>
2014-10-31 13:41:27 +0300

    fix mail sending

--- mail.php
+++ mail.php
@@ -1,5 +1,28 @@
-<?php 
-mail('tennalian@gmail.com', 'Обратный звонок c forexatlas.com'.$_SERVER['SERVER_NAME'],  
-'Здравствуйте! Меня зовут '.htmlspecialchars($_POST['name']).'. Свяжитесь со мной по номеру  '.htmlspecialchars($_POST['phone']).' Мой e-mail: '.htmlspecialchars($_POST['email']));
-die(json_encode(array('result'=>true, 'text'=>'Thank you')));  
+<?php
+
+function clean_string($string) {
+	$bad = array("content-type","bcc:","to:","cc:","href");
+	return str_replace($bad,"",$string);
+}
+$message = '';
+
+foreach (array(
+	'name',
+	'email',
+	'phone',
+) as $key) {
+	if (!empty($_POST[$key])) {
+		$message .= $key . ": ".clean_string($_POST[$key]) . PHP_EOL;
+	}
+}
+$email_to = 'nd44444@mail.ru';
+$email_from = 'robot@forexatlas.com';
+$email_subject = 'Обратный звонок c forexatlas.com'.$_SERVER['SERVER_NAME'];
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'Content-Type:text/html;charset=utf-8'."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($email_to, $email_subject, $message, $headers);
+die(json_encode(array('result'=>true, 'text'=>'Thank you')));
 ?>
